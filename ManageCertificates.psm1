30e74493da2e1d8d2caa59624caa93e2ccce5681
AstralisSomnium <gruberpatrickit@gmail.com>
2017-10-31 16:06:11 +0100

    Added module for managing certificates
    For Removing a specifc certifcicate and for getting the Store Location + Name

---
+++ ManageCertificates.psm1
@@ -0,0 +1,33 @@
+﻿# Remove-Certificate -Certificate $myCertificate -Verbose
+function Remove-Certificate {
+[CmdletBinding()]
+    param(
+        [System.Security.Cryptography.X509Certificates.X509Certificate]$certificate
+    )
+    if(-not (Test-Path -Path $certificate.PSPath)) {
+        throw "Certificate does not exist $certificate"
+    }
+    $certLocation = Get-CertificateLocation -Certificate $certificate
+    $store = New-Object System.Security.Cryptography.X509certificates.X509Store($certLocation.StoreName, $certLocation.StoreLocation)
+    try {
+        $store.Open('ReadWrite')
+        Write-Verbose -Message "Removing $certificate"
+        $store.Remove($certificate)
+    } catch {
+        Write-Verbose -Message "Failed to remove $($certificate.GetName()): $_"
+    } finally {
+        $store.close() 
+    }
+}
+
+# Get-CertificateLocation -Certificate $myCertificate
+function Get-CertificateLocation {
+    param(
+        [System.Security.Cryptography.X509Certificates.X509Certificate]$certificate
+    )
+    $segments = $certificate.PSParentPath -split "\\"
+    return New-Object -TypeName PSObject -Property @{
+        "StoreName" = ([System.Enum]::Parse([System.Security.Cryptography.X509Certificates.StoreName], $segments[2], $true))
+        "StoreLocation" = [System.Security.Cryptography.X509Certificates.StoreLocation]($segments[1] -replace "Certificate::", "")
+    }    
+}
